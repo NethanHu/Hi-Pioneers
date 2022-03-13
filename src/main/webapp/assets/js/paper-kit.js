14fd8c86f204214846580f92d2248ad94d139ffe
胡日千 <jhlxhrq@163.com>
2022-03-13 19:59:03 +0800

    手动组卷功能：备选框功能缺失

--- src/main/webapp/assets/js/paper-kit.js
+++ src/main/webapp/assets/js/paper-kit.js
@@ -1,6 +1,6 @@
-const question_course = ['', '语文', '数学', '英语', '物理'];
-const question_type = ['', '一般选择题', '简答题', '填空题', '证明题'];
-const question_level = ['', '1', '2', '3', '4', '5'];
+var question_course = ['', '语文', '数学', '英语', '物理'];
+var question_type = ['', '一般选择题', '简答题', '填空题', '证明题'];
+var question_level = ['', '1', '2', '3', '4', '5'];
 
 /**
  * 给定需要显示出的题目的各个类别，依次填充 <select> 标签内的 <option> 元素。
