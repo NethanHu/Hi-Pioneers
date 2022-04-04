2871488d3c1fda366c7aa919b2b5edae3459fc1f
胡日千 <jhlxhrq@163.com>
2022-04-04 12:11:24 +0800

    预留了试卷成绩可视化前端部分，解决了部分bug

--- src/main/java/com/jfinal/admin/result/ResultAdminController.java
+++ src/main/java/com/jfinal/admin/result/ResultAdminController.java
@@ -2,33 +2,56 @@ package com.jfinal.admin.result;
 
 import com.jfinal.admin.common.BaseController;
 import com.jfinal.admin.common.model.Exam;
+import com.jfinal.admin.common.model.Score;
 import com.jfinal.admin.test.TestAdminService;
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Path;
 import com.jfinal.plugin.activerecord.Page;
 
-@Path(value = "/admin/score", viewPath = "/admin/score")
+@Path(value = "/admin/result", viewPath = "/admin/result")
 public class ResultAdminController extends BaseController {
     @Inject
-    TestAdminService srv;
+    ResultAdminService srv;
 
     public void index() {
-        render("index.html");
-    }
-
-    public void showScore() {
         int accountId = getLoginAccountId();
         int roleId = srv.getRoleId(accountId);
-        if (roleId == 4) { // 学生
-            String StudentNo = srv.getStuNo(accountId);
+        if (roleId == 4) { // 仅对于学生
+            String StudentNo = srv.getAccNo(accountId);
             String[] course_name = srv.getStuCourse(StudentNo);
-            Page<Exam> page = srv.studentPaginate(getInt("pn", 1), course_name);
+            Page<Score> page = srv.studentPaginate(getInt("pn", 1), StudentNo);
             set("page", page);
-            render("index.html");
-        } else if (roleId == 5) { // 老师
-
-        } else if (roleId == 6) { // 学科主任
-
+            render("student_index.html");
+        }
+        if (roleId==5){
+            String TeacherNo = srv.getAccNo(accountId);
+            String[] course_name = srv.getTeacherCourse(TeacherNo);
+            Page<Score> page = srv.teacherPaginate(getInt("pn", 1),course_name);
+            set("page", page);
+            render("teacher_index.html");
+        }
+        if (roleId==6){
+            String headNo = srv.getAccNo(accountId);
+            String name = srv.getHeaderCourse(headNo);
+            Page<Score> page = srv.headerPaginate(getInt("pn", 1),name);
+            set("page", page);
+            render("header_index.html");
         }
     }
+
+//    public void showScore() {
+//        int accountId = getLoginAccountId();
+//        int roleId = srv.getRoleId(accountId);
+//        if (roleId == 4) { // 学生
+//            String StudentNo = srv.getAccNo(accountId);
+//            String[] course_name = srv.getStuCourse(StudentNo);
+//            Page<Score> page = srv.studentPaginate(getInt("pn", 1), course_name);
+//            set("page", page);
+//            render("index.html");
+//        } else if (roleId == 5) { // 老师
+//
+//        } else if (roleId == 6) { // 学科主任
+//
+//        }
+//    }
 }
--- src/main/java/com/jfinal/admin/result/ResultAdminService.java
+++ src/main/java/com/jfinal/admin/result/ResultAdminService.java
@@ -11,11 +11,12 @@ import java.util.List;
 public class ResultAdminService {
     private static int pageSize = 15;
 
-    private Exam dao = new Exam().dao();
+    private Score dao = new Score().dao();
     private Paper Pdao = new Paper().dao(); // 在 Exam 模块中引入 Paper 的数据库查询功能
     private Question Qdao = new Question().dao(); // 在 Exam 模块中引入 Question 的数据库查询功能
     private CourseSelection CSdao = new CourseSelection().dao();
     private Course Cdao = new Course().dao();
+    private Teaching Tdao = new Teaching().dao();
 
 
     /**
@@ -26,7 +27,7 @@ public class ResultAdminService {
         return Db.queryInt(sql);
     }
 
-    public String getStuNo(int accountId) {
+    public String getAccNo(int accountId) {
         String sql = "select number from account where id =" + accountId + " limit 1";
         return Db.queryStr(sql);
     }
@@ -56,22 +57,65 @@ public class ResultAdminService {
         return course_name;
     }
 
-    public Page<Exam> paginate(int pageNumber) {
+    public String[] getTeacherCourse(String Tno) {
+        String sql = "select distinct Cno from teaching where Tno = '" + Tno + "'";
+        List<Teaching> Teaching = Tdao.find(sql);
+        String[] course = new String[Teaching.size()];
+        for (int i = 0; i < Teaching.size(); i++) {
+            Teaching Teach = Teaching.get(i);
+            course[i] = Teach.getCno();
+        }
+        String[] course_name = new String[Teaching.size()];
+        String sql_name = "select distinct name from Course where ";
+        for (int i = 0; i < Teaching.size(); i++) {
+            if (i == 0) {
+                sql_name = sql_name + "Cno = '" + course[i] + "' ";
+            } else {
+                sql_name = sql_name + " or Cno ='" + course[i] + "' ";
+            }
+        }
+        List<Course> courseList = Cdao.find(sql_name);
+        for (int i = 0; i < courseList.size(); i++) {
+            Course courseNname = courseList.get(i);
+            course_name[i] = courseNname.getName();
+        }
+        return course_name;
+    }
+
+    public String getHeaderCourse(String HNo) {
+        String sql = "select Cno from teaching where Tno = '" + HNo + "'";
+        String Cno = Db.queryStr(sql);
+        String sql_type = "select type from Course where Cno = '"+Cno+"'";
+        String type = Db.queryStr(sql_type);
+        return type;
+    }
+
+    public Page<Score> paginate(int pageNumber) {
         return dao.paginate(pageNumber, pageSize, "select *", "from exam where state=1 order by update_time desc");
     }
 
-    public Page<Exam> studentPaginate(int pageNumber, String[] name) {
-        String sql = "from exam where";
+    public Page<Score> teacherPaginate(int pageNumber, String[] name) {
+        String sql = "from Score where";
         for (int i = 0; i < name.length; i++) {
             if (i == 0) {
-                sql = sql + " course_name = '" + name[i] + "' ";
+                sql = sql + " type = '" + name[i] + "' ";
             } else {
-                sql = sql + "or course_name = '" + name[i] + "' ";
+                sql = sql + "or type = '" + name[i] + "' ";
             }
         }
-        sql = sql + " order by start_time ";
-        return dao.paginate(pageNumber, pageSize, "select *", sql);
+        sql = sql + " order by updateTime ";
+        return dao.paginate(pageNumber, pageSize, "select * ", sql);
     }
+    public Page<Score> headerPaginate(int pageNumber, String type){
+        String sql = "from Score where type='高等数学' or type='线性代数' or type='概率论'";
+        return dao.paginate(pageNumber, pageSize,"select * ",sql);
+    }
+
+    public Page<Score> studentPaginate(int pageNumber, String studentNo) {
+        String sql = "from Score where studentId='" + studentNo + "'";
+        return dao.paginate(pageNumber, pageSize, "select * ", sql);
+    }
+
 
     public Page<Paper> Ppaginate(int pageNumber) {
         return Pdao.paginate(pageNumber, pageSize, "select *", "from Paper order by update_time desc");
@@ -80,7 +124,7 @@ public class ResultAdminService {
     /**
      * 获取 id
      */
-    public Exam EgetById(int id) {
+    public Score SgetById(int id) {
         return dao.findById(id);
     }
 
